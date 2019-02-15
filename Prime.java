c6b62280d223a9cb72c84acb538398ecc9d905f6
sangameshwar2804 <45010542+sangameshwar2804@users.noreply.github.com>
2019-02-15 11:14:09 -0800

    updated

--- Prime.java
+++ Prime.java
@@ -1,7 +1,33 @@
 class Prime
 {
+  public void add()
+  {
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println(c);
+  }
+  public void sub()
+  {
+    int a=10;
+    int b=20;
+    int c=a-b;
+    System.out.println(c);
+  }
+public void mul()
+  {
+    int a=10;
+    int b=20;
+    int c=a*b;
+    System.out.println(c);
+  }
+
 public static void main(String[] args)
 {
+  Prime p=new Prime();
+  p.add();
+  p.sub();
+  p.mul();
 System.out.println("Sample");
 }
 }
